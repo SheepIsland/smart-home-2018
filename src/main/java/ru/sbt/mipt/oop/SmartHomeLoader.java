09e93f1555c0512d8da0b183af3445d0ba0b1812
SheepIsland <violettasheep@gmail.com>
2018-10-05 00:59:28 +0300

    initial
    initial

---
+++ src/main/java/ru/sbt/mipt/oop/SmartHomeLoader.java
@@ -0,0 +1,11 @@
+package ru.sbt.mipt.oop;
+
+import java.io.IOException;
+
+/**
+ * Created by Violetta on 04/10/2018.
+ */
+public interface SmartHomeLoader {
+    SmartHome loadSmartHome() throws IOException;
+
+}
